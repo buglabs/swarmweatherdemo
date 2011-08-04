45893ab35ef589eedcf4ee7ce54b6bd25d90f526
Henry <henry.liu@buglabs.net>
2011-08-04 16:57:03 -0400

    deleted old feedtest

--- BUGSwarmTest/webinterface/helloworld.js
+++
@@ -1,7 +0,0 @@
-function hello() {
- document.getElementById('hello').innerHTML = 'Hello World';
-}
-function start() {
- document.getElementById('click').onclick = hello;
-}
-window.onload = start;
